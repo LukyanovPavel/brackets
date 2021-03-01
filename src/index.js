f9d36624b26114ace7a84f94322c4659a64e9b82
ars2 <ars2@MacBook-Pro-ars2.local>
2021-03-01 14:56:26 +0300

    add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  bracketsConfig = bracketsConfig.join().split(',');
+  let open = [],
+  close = [], 
+  stack = [];
+  
+  bracketsConfig.forEach((element, index) => {
+    if (index % 2 === 0){
+      open.push(element);
+    } else{
+      close.push(element);
+    }
+  });
+  
+  str = str.split('');
+  for (let i = 0; i < str.length; i++){
+    let openIndex = open.indexOf(str[i]);
+    if (openIndex !== -1){
+      stack.push(openIndex);
+    }
+    let closeIndex = close.indexOf(str[i]);
+    if (closeIndex !== -1){
+      openIndex = stack.pop();
+      if (openIndex !== closeIndex) return false;
+    }
+  }
+  return stack.length !== 0 ? false : true;
+  }
+  
